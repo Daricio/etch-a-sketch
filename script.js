aad6e509bd8465c0f2736cd7f6d1bd0f36f89264
Daricio <140209305+Daricio@users.noreply.github.com>
2023-08-22 16:37:31 +0300

    Add creation of new grid of requested size

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 const container = document.getElementById('container');
 
-let squaresPerSide = 16;
+createGrid(16);
 
 // when button is clicked, prompt number of squares per side
-// const newGridButton = document.getElementById('new-grid-btn');
-// newGridButton.addEventListener('click', () => {
-//   let newSquaresPerSide = prompt('Enter number of squares per side:');
+const newGridButton = document.getElementById('new-grid-btn');
+newGridButton.addEventListener('click', () => {
+  let squaresPerSide = prompt('Enter number of squares per side (100 max):');
 
-//   if(!newSquaresPerSide) return;
+  if (squaresPerSide === null || squaresPerSide === '' || isNaN(+squaresPerSide)) return;
+  if (squaresPerSide > 100 ) {
+    alert('Maximum number of cells per side is 100.');
+    return;
+  }
 
-//   // remove previous grid
-//   removeGrid();
+  // remove previous grid
+  removeGrid();
 
-//   // create new grid of requested size
-//   createGrid(newSquaresPerSide);
+  // create new grid of requested size
+  createGrid(squaresPerSide);
 
-// })
+})
 
 
 // create grid
-let numOfCells = squaresPerSide ** 2;
-for (let i = 0; i < numOfCells; i++) {
-  let gridCell = document.createElement('div');
-  container.appendChild(gridCell);
+function createGrid(squaresPerSide) {
+  // create requested number of squares of requested size
+  // set width of container children
+  if (squaresPerSide < 1) return;
+  const cellWidth = 1 / squaresPerSide;
+
+  let numOfCells = squaresPerSide ** 2;
+  for (let i = 0; i < numOfCells; i++) {
+    let gridCell = document.createElement('div');
+    gridCell.style.width = `${cellWidth * 100}%`;
+    container.appendChild(gridCell);
+  }
+
+  addHoveringEffect();
 }
 
+
 // add event listener for each grid div to paint black
-const gridCells = document.querySelectorAll('.container > div');
-gridCells.forEach(cell => {
-  cell.addEventListener('mouseover', paintBlack);
-})
+function addHoveringEffect() {
+  const gridCells = document.querySelectorAll('.container > div');
+  gridCells.forEach(cell => {
+    cell.addEventListener('mouseover', paintBlack);
+  })
+}
+
 
 function paintBlack() {
   this.classList.add('black');
